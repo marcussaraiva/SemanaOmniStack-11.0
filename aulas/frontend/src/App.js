a276e5be1e493e4857eb0139b188329a5be9bd67
Marcus Saraiva <55753204+marcussaraiva@users.noreply.github.com>
2020-03-24 20:46:04 -0300

    Commit

--- aulas/frontend/src/App.js
+++ aulas/frontend/src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello Omnistack!
+          Hello Omnistack
         </p>
         <a
           className="App-link"
