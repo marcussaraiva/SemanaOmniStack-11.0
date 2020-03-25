b54ee92fa42600a1751f6dd2c8a809d188f69f00
Marcus Saraiva <55753204+marcussaraiva@users.noreply.github.com>
2020-03-25 13:45:06 -0300

    Dia 2

---
+++ aulas/backend/src/index.js
@@ -0,0 +1,32 @@
+// Arquivo principal da aplicação
+const express = require('express'); // Importando Express (pacote)
+const cors = require('cors'); 
+const routes = require('./routes'); // Importando Routes (pasta)
+
+
+const app = express();
+
+app.use(cors());
+app.use(express.json()); // Converter as requisiçoes em um JSON
+app.use(routes); // Tem que ser abaixo do 'app.use(express.json());'
+
+
+/* 
+    METODOS HTTP:
+
+    GET:    Buscar/listar uma informação do back-end
+    POST:   Criar uma informaçao no back-end 
+    PUT:    Alterar uma informação no back-end
+    DELETE: Deletar uma informação no back-end 
+ */
+
+ /* 
+    TIPOS DE PARAMETROS:
+
+    QUERY PARAMS: Parâmetros nomeados enviados na rota após '?' (filtros, paginação)
+    ROUTE PARAMS: Parâmetros utilizados para identificar recursos
+    REQUEST BODY: Corpo da requisição, utilizado para criar ou alterar recursos
+
+ */
+
+app.listen(3333);   //Qual porta a aplicaçao vai ouvir
